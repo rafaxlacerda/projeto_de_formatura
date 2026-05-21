f1827bce05f6de34ae2de6712471f9137e5d88a0
Ana Júlia Morais <anaj.moraisf@gmail.com>
2026-05-21 01:31:53 -0300

    Included new grahphs to analytics

--- 1.4_geracao_de_cenarios/sortear_monte_carlo.py
+++ 1.4_geracao_de_cenarios/sortear_monte_carlo.py
@@ -23,7 +23,7 @@ CARGA_PICO_TOTAL_MW = 1.769
 
 # PARÂMETROS DO SORTEIO MONTE CARLO
 
-N_REALIZACOES = 200     # Sorteios por nível de penetração
+N_REALIZACOES = 50     # Sorteios por nível de penetração
 N_HORAS = 24            # Resolução temporal
 
 # Range de Penetração FV: de 0% a 150% em passos de 10%
